0bd67f52dcec96284b6f4963b7384fe98849ff76
Hamish Meikle <hwhmeikle@hotmail.com>
2016-09-19 01:22:07 +1200

    Added a bunch of relevant classes to the .net package.
    At this point everything is very primitive, and definitely subject to
    change.

--- src/swen222/niwa/net/Master.java
+++ src/swen222/niwa/net/Master.java
@@ -1,5 +1,32 @@
 package swen222.niwa.net;
 
+import java.net.Socket;
+
+/**
+ * A master connection receives events from a slave connection via a socket.
+ * These events are registered within the game. The master connection is also
+ * responsible for transmitting information to the slave about the current game
+ * state.
+ *
+ * @author Hamish M
+ *
+ */
 public class Master {
+	
+	// TODO: Need a reference to the game
+	private final int broadcastClock;
+	private final int uid; // a unique id
+	private final Socket socket;
+	
+	public Master(int broadcastClock, int uid, Socket socket) {
+		this.broadcastClock = broadcastClock;
+		this.uid = uid;
+		this.socket = socket;
+	}
+	
+	public void run(){
+		// TODO: While the game is active, this method should be continuously running. It should
+		// be receiving information from clients and then broadcasting it back to the client.
+	}
 
 }
---
+++ src/swen222/niwa/net/Slave.java
@@ -0,0 +1,15 @@
+package swen222.niwa.net;
+
+/**
+ * A slave connection receives information about the current state of the board
+ * and relays that into the local copy of the board. The slave connection also
+ * notifies the master connection of key presses by the player.
+ * 
+ * @author Hamish M
+ */
+
+public class Slave extends Thread{
+	
+	//TODO: Very much just a place holder at the moment, will obviously need further implementation
+
+}
