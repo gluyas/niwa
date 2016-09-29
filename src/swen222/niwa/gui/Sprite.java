5a20f75e69084a2c45961a77382d1974fda58803
ElectricPowerHouse <acidplaystf2@gmail.com>
2016-09-29 18:25:49 +1300

    Updated sprite class to scale
    Sprite class now scales with a given 'blockSize'

--- src/swen222/niwa/gui/Sprite.java
+++ src/swen222/niwa/gui/Sprite.java
@@ -1,6 +1,8 @@
 package swen222.niwa.gui;
 
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.awt.image.RescaleOp;
 
 /**
  * Simple class containing an Image with an anchor point. Used by the Visible interface and GUI.
@@ -12,18 +14,24 @@ public class Sprite {
 	public final Image img; //
 	public final int aX;
 	public final int aY;
+	
 
 	/**
 	 * Create a new Sprite from a given Image, with an anchor point relative to it.
 	 * @param img   the Image for this Sprite to represent
 	 * @param aX    left (x) co-ordinate of the anchor point
 	 * @param aY    top  (y) co-ordinate of the anchor point
+	 * @param width  the width of the sprite relative to blocks e.g. width of 1 = 1 block
 	 */
 	public Sprite(Image img, int aX, int aY) {
 		this.img = img;
 		this.aX = aX;
 		this.aY = aY;
+		
 	}
+	
+	
+	
 
 	/**
 	 * Draws this Sprite onto a specified Graphics object, anchored at given co-ordinates
@@ -31,8 +39,17 @@ public class Sprite {
 	 * @param x left position of the anchor
 	 * @param y top position of the anchor
 	 */
-	public void draw(Graphics g, int x, int y) {
-		g.drawImage(img, x - aX, y - aY, null);
+	public void draw(Graphics g, int x, int y, int blockSize) {
+		 
+		//int imgWidth = img.getWidth(null);
+		int imgHeight = img.getHeight(null);
+		
+		int scaledWidth = blockSize;
+		int scaledHeight = (scaledWidth/blockSize)*imgHeight; 
+		int scaledAX = aX*blockSize;
+		int scaledAY = aY*blockSize;
+		
+		g.drawImage(img, x - scaledAX, y - scaledAY, scaledWidth, scaledHeight, null);
 	}
 
 }
