90633aef00d062271347c5e9fd6b18f7d80ec654
burnshami <burnshami@malacca.ecs.vuw.ac.nz>
2016-10-08 17:21:20 +1300

    Rune stones and testing

---
+++ src/swen222/niwa/model/entity/Rune.java
@@ -0,0 +1,18 @@
+package swen222.niwa.model.entity;
+
+import swen222.niwa.gui.Sprite;
+import swen222.niwa.model.world.Direction;
+import swen222.niwa.model.world.Location;
+
+public class Rune extends ObjectEntity {
+
+	public Rune (Location loc){
+		super(loc);
+	}
+
+	@Override
+	public Sprite sprite(Direction facing) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
