02cc281128eb83990d2bd8ca975143bbd7cd4770
Hamish Meikle <hwhmeikle@hotmail.com>
2016-10-10 01:42:42 +1300

    Finished off the SpriteLoader class.
    Everything compiles and seems to work with very limited testing.

--- src/swen222/niwa/file/SpriteLoader.java
+++ src/swen222/niwa/file/SpriteLoader.java
@@ -1,14 +1,18 @@
 package swen222.niwa.file;
 
+import java.awt.Image;
 import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.imageio.ImageIO;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import swen222.niwa.gui.Sprite;
@@ -24,110 +28,237 @@ import swen222.niwa.model.world.Direction;
  */
 public class SpriteLoader {
 
-    public static final String manifest = "resource/sprites/spritesets.xml";
+	public static final String manifest = "resource/images/spritesets.xml";
 
-    private static final Map<String, SpriteSet> SPRITESETS = new HashMap<>();
+	private static final Map<String, SpriteSet> SPRITESETS = new HashMap<>();
 
-    static {
-        try {
-            // initial set up
-            File file = new File(manifest);
-            DocumentBuilder dBuilder = DocumentBuilderFactory.newInstance()
-                    .newDocumentBuilder();
-            Document doc = dBuilder.parse(file);
+	static {
+		try {
+			// initial set up
+			File file = new File(manifest);
+			DocumentBuilder dBuilder = DocumentBuilderFactory.newInstance()
+					.newDocumentBuilder();
+			Document doc = dBuilder.parse(file);
 
-            doc.getDocumentElement().normalize();
+			doc.getDocumentElement().normalize();
 
-            // get the manifest node and its children
-            NodeList manifestNode = doc.getElementsByTagName("manifest");
-            NodeList nodeList = manifestNode.item(0).getChildNodes();
+			// get the manifest node and its children
+			NodeList manifestNode = doc.getElementsByTagName("manifest");
+			NodeList nodeList = manifestNode.item(0).getChildNodes();
 
-            // iterate over each node and start parsing
-            for (int i = 0; i < nodeList.getLength() ; i++) {
-                Node currentNode = nodeList.item(i);
-                String nodeName = currentNode.getNodeName();
+			// iterate over each node and start parsing
+			for (int i = 0; i < nodeList.getLength() ; i++) {
+				Node currentNode = nodeList.item(i);
+				String nodeName = currentNode.getNodeName();
 
-                switch (nodeName){
-                    case "spriteset":
-                        parseSpriteSet(currentNode, new HashMap<>());
-                        break;
-                    case "group":
-                        parseGroup(currentNode, new HashMap<>());
-                        break;
-                }
-            }
-
-
-        } catch (Exception e) {
-            throw new Error(e);
-        }
-    }
-
-    private static void parseGroup(Node groupNode, Map<String, Sprite> context){
-        Map<String, Sprite> localContext = new HashMap<>(context);
-        NodeList children =  groupNode.getChildNodes();
-
-        // iterate over each child
-        for(int i = 0 ; i < children.getLength(); i++){
-            Node currentNode = children.item(i);
-            Element child = (Element) currentNode;
-
-            // decide what to do with each child
-            switch (child.getNodeName()){
-                case "sprite":
-                    localContext.put(child.getAttribute("id"), parseSprite(currentNode));
-                    break;
-                case "group":
-                    parseGroup(currentNode, localContext);
-                    break;
-                case "spriteset":
-                    parseSpriteSet(currentNode, localContext);
-                    break;
-            }
-        }
-    }
-
-    private static void parseSpriteSet(Node spriteSetNode, Map<String, Sprite> context){
-        Map<String, Sprite> localContext = new HashMap<>(context);
-
-        // check if it has a facing element
-        // if not then just create a SS from the first Sprite element
-        // otherwise iterate over sprites and add them to localContext
-        // then parse faces; add result to spritesets under the name attribute
-    }
-
-    private static Sprite parseSprite(Node spriteNode){
-        //make new SpriteSet from the node
-        //set fields based on faces element's attributes
-        // or if sprite node has no id element then set it to all faces
-        return null;
-    }
-    
-    private static SpriteSet parseFaces(Node facesNode, Map<String, Sprite> context) {
-        //create new SS
-        //assign sprite indicies by the faces' attributes
-        return null;
-    }
+				switch (nodeName){
+				case "spriteset":
+					parseSpriteSet(currentNode, new HashMap<>());
+					break;
+				case "group":
+					parseGroup(currentNode, new HashMap<>());
+					break;
+				}
+			}
 
 
-    static class SpriteSet implements Visible {
-        private Sprite[] sprites = new Sprite[4];
+		} catch (Exception e) {
+			throw new Error(e);
+		}
+	}
 
-        public SpriteSet() {
+	private static void parseGroup(Node groupNode, Map<String, Sprite> context){
+		Map<String, Sprite> localContext = new HashMap<>(context);
+		NodeList children =  groupNode.getChildNodes();
 
-        }
+		// iterate over each child
+		for(int i = 0 ; i < children.getLength(); i++){
+			Node currentNode = children.item(i);
+			
+			// make sure we're not casting a text node to an Element
+			if(currentNode.getNodeType() == Node.ELEMENT_NODE){
+				Element child = (Element) currentNode;
 
-        public SpriteSet(Sprite sprite) {
-            for (int i = 0; i < 4; i++) sprites[i] = sprite;
-        }
+				// decide what to do with each child
+				switch (child.getNodeName()){
+				case "sprite":
+					localContext.put(child.getAttribute("id"), parseSprite(currentNode));
+					break;
+				case "group":
+					parseGroup(currentNode, localContext);
+					break;
+				case "spriteset":
+					parseSpriteSet(currentNode, localContext);
+					break;
+				}
+			}
+		}
+	}
 
-        @Override
-        public Sprite sprite(Direction facing) {
-            return sprites[facing.ordinal()];
-        }
-    }
+	private static void parseSpriteSet(Node spriteSetNode, Map<String, Sprite> context){
+		Map<String, Sprite> localContext = new HashMap<>(context);
+		String ssName = ((Element) spriteSetNode).getAttribute("name");
+		NodeList children = spriteSetNode.getChildNodes();
 
-    public static void main(String[] args){
-        new SpriteLoader();
-    }
+		// check if the SpriteSet has a faces node
+		if(contains(children, "faces")){
+			// iterate over children and parse sprites into localContext
+			for(int i = 0 ; i < children.getLength(); i++){
+				Node currentNode = children.item(i);
+				
+				// make sure we're not casting a text node to an Element
+				if(currentNode.getNodeType() == Node.ELEMENT_NODE){
+					Element child = (Element) currentNode;
+
+					if(child.getNodeName().equals("sprite")){
+						localContext.put(child.getAttribute("id"), parseSprite(currentNode));
+					}
+				}
+			}
+
+			// then parse the faces node; add result to SPRITESETS
+			SpriteSet spriteSet = parseFaces(get(children, "faces"), localContext);
+			SPRITESETS.put(ssName, spriteSet);
+		} 
+		// else create a SpriteSet from the first Sprite element
+		// and add it to SPRITESETS
+		else{
+			SpriteSet spriteSet = new SpriteSet(parseSprite(get(children, "sprite")));
+			SPRITESETS.put(ssName, spriteSet);
+		}
+	}
+
+	private static Sprite parseSprite(Node spriteNode){
+		Element spriteElement = (Element)spriteNode;
+		try {
+			// each sprite will always have an image path
+			Image img = ImageIO.read(new File(spriteElement.getAttribute("img")));
+			
+			// now store the other potential attributes; ax, ay, width
+			String axAttr = spriteElement.getAttribute("ax");
+			String ayAttr = spriteElement.getAttribute("ay");
+			String widthAttr =  spriteElement.getAttribute("width");
+			
+			// now check which attributes have been specified, if they are
+			// empty then assign them default values.
+			double ax, ay, width;
+			if(axAttr.isEmpty()){ ax = 0.5;}
+			else{ ax = Double.parseDouble(axAttr);}
+			
+			if(ayAttr.isEmpty()){ ay = 0.25;}
+			else{ ay = Double.parseDouble(ayAttr);}
+			
+			if(widthAttr.isEmpty()){ width = 1;}
+			else{ width = Double.parseDouble(widthAttr);}
+			
+			// now create the Sprite
+			return new Sprite(img, ax, ay, width);
+		} catch (IOException e) {
+			System.out.println("A sprite tag must have an image attribute");
+			e.printStackTrace();
+		}
+
+
+		return null;
+	}
+
+	private static SpriteSet parseFaces(Node facesNode, Map<String, Sprite> context) {
+		SpriteSet ss = new SpriteSet();
+		Element facesElement = (Element) facesNode;
+		
+		// north face
+		String northFace = facesElement.getAttribute("n");
+		Sprite northSprite = context.get(northFace);
+		ss.setSprite(Direction.NORTH, northSprite);
+
+		// east face
+		String eastFace = facesElement.getAttribute("e");
+		Sprite eastSprite = context.get(eastFace);
+		ss.setSprite(Direction.EAST, eastSprite);
+		
+		// south face
+		String southFace = facesElement.getAttribute("s");
+		Sprite southSprite = context.get(southFace);
+		ss.setSprite(Direction.SOUTH, southSprite);
+		
+		// west face
+		String westFace = facesElement.getAttribute("w");
+		Sprite westSprite = context.get(westFace);
+		ss.setSprite(Direction.WEST, westSprite);
+		
+		return ss;
+	}
+	
+
+
+	/**
+	 * Contains returns whether or not a NodeList contains a type of Node.
+	 * @param nList The NodeList to search through.
+	 * @param nodeName The Node type to find.
+	 * @return true if the list contains the node, else false.
+	 */
+	public static boolean contains(NodeList nList, String nodeName){
+		for(int i = 0 ; i < nList.getLength() ;  i++){
+			Node currentNode = nList.item(i);
+			if(currentNode.getNodeName().equals(nodeName)){
+				return true;
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * Gets the first appearance of a specified Node type in a NodeList.
+	 * @param nList The NodeList to search through.
+	 * @param nodeName The Node type to get.
+	 * @return A Node object if the node is found, else null.
+	 */
+	public static Node get(NodeList nList, String nodeName){
+		for(int i = 0 ; i < nList.getLength() ;  i++){
+			Node currentNode = nList.item(i);
+			if(currentNode.getNodeName().equals(nodeName)){
+				return currentNode;
+			}
+		}
+		return null;	
+	}
+	
+	// method is here for debugging purposes
+	public Map<String, SpriteSet> getMap(){
+		return SPRITESETS;
+	}
+
+	/**
+	 * A SpriteSet holds images for an entity/prop. It stores an image
+	 * for each facing direction; north, east, south, west. 
+	 * 
+	 * @author Hamish M
+	 *
+	 */
+	static class SpriteSet implements Visible {
+		private Sprite[] sprites = new Sprite[4];
+
+		public SpriteSet() {}
+
+		public SpriteSet(Sprite sprite) {
+			for (int i = 0; i < 4; i++) sprites[i] = sprite;
+		}
+		
+		public void setSprite(Direction face, Sprite sprite){
+			sprites[face.ordinal()] = sprite;
+		}
+		
+		@Override
+		public Sprite sprite(Direction facing) {
+			return sprites[facing.ordinal()];
+		}
+	}
+
+	public static void main(String[] args){
+		SpriteLoader sl = new SpriteLoader();
+		Map<String, SpriteSet> map = sl.getMap();
+		for(String s : map.keySet()){
+			System.out.println(s);
+		}
+	}
 }
