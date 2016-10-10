b56870fff1996c1af48a7cd6f41d0db4c22c8a36
Jack Upton <uptonjack@daawat.ecs.vuw.ac.nz>
2016-10-11 12:14:10 +1300

    Updated tests to get rid of errors

--- src/swen222/niwa/gui/InventoryBtn.java
+++ src/swen222/niwa/gui/InventoryBtn.java
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 
 package swen222.niwa.gui;
 
@@ -126,3 +127,133 @@ public class InventoryBtn extends JToggleButton implements MouseListener {
 	}
 
 }
+=======
+
+package swen222.niwa.gui;
+
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+
+/**
+ * Represents an item in the inventory
+ * 
+ * @author Zoe
+ *
+ */
+public class InventoryBtn extends JToggleButton implements MouseListener {
+
+	private boolean hover;
+	private String item;// name of the item in this slot, "Empty" if nothing
+						// there
+	public static final int SIZE = 50;
+	private Color color;// stand in for thumb nail image, for testing purposes
+	private Image img;
+	private int count;// how many of this kind of item the player has
+
+	public InventoryBtn() {
+		item = "Empty";
+		count = 0;
+		hover = false;
+		// this is just here to test the drawing, will eventually take img from
+		// sprite
+		try {
+			//img = ImageIO.read(new File("resource/images/entities/misc/seedThumb.png"));
+			img = ImageIO.read(new File("resource/images/entities/runes/1Thumb.png"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		color = Color.WHITE;
+
+		setFocusable(true);
+		setContentAreaFilled(false);
+		setBorderPainted(false);
+		setVisible(true);
+		addMouseListener(this);
+
+		setActionCommand(item);
+		setToolTipText(item + ", " + count);
+	}
+
+	/**
+	 * updates the item and count fields, tool tip and action command of the
+	 * button
+	 * 
+	 * @param item
+	 *            - the new item String
+	 * @param count
+	 *            - the new count integer
+	 */
+	public void updateButton(String item, int count, Image img) {
+		this.item = item;
+		this.count = count;
+		this.img = img;
+		setActionCommand(item);
+		setToolTipText(item + ", " + count);
+	}
+
+	public void setColor(Color color) {
+		this.color = color;
+	}
+
+	@Override
+	public Dimension getPreferredSize() {
+		return new Dimension(SIZE, SIZE);
+	}
+
+	@Override
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+
+		int x = this.getWidth() / 2 - SIZE / 2;
+		int y = this.getHeight() / 2 - SIZE / 2;
+
+		if (this.isSelected()) {
+			g.setColor(new Color(0xF27165));
+		} else if (hover) {
+			g.setColor(new Color(0xA9EF6B));
+		} else {
+			g.setColor(new Color(0xA4E2E8));
+		}
+		g.fillRect(x, y, SIZE, SIZE);
+
+		g.setColor(color);
+		g.fillRect(x + SIZE / 10, y + SIZE / 10, SIZE - 2 * (SIZE / 10), SIZE - 2 * (SIZE / 10));
+		g.drawImage(img, x + SIZE / 10, y + SIZE / 10, SIZE - 2 * (SIZE / 10), SIZE - 2 * (SIZE / 10), null);
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		hover = true;
+		repaint();
+
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		hover = false;
+		repaint();
+
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+	}
+
+}
+>>>>>>> Stashed changes
--- src/swen222/niwa/model/entity/entities/PlayerEntity.java
+++ src/swen222/niwa/model/entity/entities/PlayerEntity.java
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 package swen222.niwa.model.entity.entities;
 
 import java.util.ArrayList;
@@ -75,3 +76,80 @@ public class PlayerEntity extends Entity{
 
 	}
 }
+=======
+package swen222.niwa.model.entity.entities;
+
+import java.util.ArrayList;
+
+import swen222.niwa.gui.Sprite;
+import swen222.niwa.model.entity.Entity;
+import swen222.niwa.model.world.Direction;
+import swen222.niwa.model.world.Location;
+
+/**
+ * @author burnshami
+ *
+ */
+public class PlayerEntity extends Entity{
+
+	public String name;
+	public int points;
+	public ArrayList<Entity> inventory;
+	public int inventoryCapacity;
+	public Direction facing;
+
+	public PlayerEntity(Location loc) {
+		super(loc);
+		inventory= new ArrayList<Entity>();
+		inventoryCapacity=3;
+		points=0;
+	}
+
+	@Override
+	public Sprite sprite(Direction camera) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void addItem(Entity item){
+		inventory.add(item);
+	}
+	
+	public String getName(){
+		return name;
+	}
+	
+	public int getPoints(){
+		return points;
+	}
+
+	public void removeItem(Entity item){
+		if (inventory.remove(item)) {
+			setChanged();
+			//notifyObservers()
+		}
+	}
+	public Direction getFacing(){
+		return facing;
+	}
+
+	public void updateFacing(Direction dir){
+		facing=dir;
+	}
+
+	public void addPoints(int number){
+		while(number!=0){
+			points++;
+			number--;
+		}
+	}
+
+	public boolean canPickUp(){
+		if(inventory.size()==inventoryCapacity){
+			return false;
+		}
+		return true;
+
+	}
+}
+>>>>>>> Stashed changes
