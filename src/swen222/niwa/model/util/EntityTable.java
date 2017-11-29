f35b804e175732b46fb78d755ad8477e2c979862
Marc <marc.gluyas@gmail.com>
2017-11-30 10:58:10 +1300

    Added Class-specific Getters to EntityTable

--- src/swen222/niwa/model/util/EntityTable.java
+++ src/swen222/niwa/model/util/EntityTable.java
@@ -7,6 +7,8 @@ import java.io.Serializable;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.Set;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
 
 /**
  * Set subinterface that provides easy access to stored Entities by their Location.
@@ -16,7 +18,7 @@ import java.util.Set;
 public interface EntityTable<T extends Entity> extends Set<T>, Serializable {
 
 	/**
-	 * Get all Entities stored in this table at a specified location. Note that the members of this Collection
+	 * Get all Entities stored in this table at a specified loc. Note that the members of this Collection
 	 * may change over time. This method makes two guarantees: that at the time of being called, all members of
 	 * this Set with the specified Location will be present in the Collection; and that all future members of the
 	 * Collection will also have that Location.
@@ -26,8 +28,17 @@ public interface EntityTable<T extends Entity> extends Set<T>, Serializable {
 	 */
 	Set<T> get(Location loc);
 
-	static <T extends Entity> EntityTable<T> unmodifiable(EntityTable<T> et) {
-		return new UnmodifiableEntityTable<>(et);
+	@SuppressWarnings("unchecked")
+	default <U extends T> Stream<U> get(Location loc, Class<? extends U> clazz) {
+		return (Stream<U>) this.get(loc).stream().filter(clazz::isInstance);
+	}
+
+	default <U extends T> U getAny(Location loc, Class<? extends U> clazz) {
+		return get(loc, clazz).findAny().orElse(null);
+	}
+
+	default <U extends T> U getAny(Location loc, Class<? extends U> clazz, Predicate<? super U> filter) {
+		return get(loc, clazz).filter(filter).findAny().orElse(null);
 	}
 
 	/**
